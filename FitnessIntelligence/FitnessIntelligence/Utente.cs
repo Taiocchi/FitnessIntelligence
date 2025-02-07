827e2a56597b365b3ef3eac9690825b4c5c22144
TAIOCCHI LUCA <TAIOCCHI.21206>
2025-02-07 08:56:40 +0100

    Versione 1

---
+++ FitnessIntelligence/FitnessIntelligence/Utente.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Utente
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Nome { get; set; }
+    public int Età { get; set; }
+    public string Ruolo { get; set; } // Studente, Docente, Allenatore
+}
---
+++ FitnessIntelligence/FitnessIntelligence/UtentiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UtentiController : ControllerBase
+{
+    private readonly IMongoCollection<Utente> _utentiCollection;
+
+    public UtentiController()
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("PalestraDB");
+        _utentiCollection = database.GetCollection<Utente>("Utenti");
+    }
+
+    [HttpGet]
+    public async Task<List<Utente>> Get() =>
+        await _utentiCollection.Find(u => true).ToListAsync();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Utente nuovoUtente)
+    {
+        await _utentiCollection.InsertOneAsync(nuovoUtente);
+        return CreatedAtAction(nameof(Get), new { id = nuovoUtente.Id }, nuovoUtente);
+    }
+}
